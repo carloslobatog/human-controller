ab0d856943a0b77b1dc85b86dd1b27efa9125692
carloslobatog <carloslobatog@gmail.com>
2017-04-05 11:34:27 +0200

    something changed O.O

---
+++ humanController/src/main.cpp
@@ -0,0 +1,301 @@
+/*
+ *    Copyright (C) 2017 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/** \mainpage RoboComp::human_controller
+ *
+ * \section intro_sec Introduction
+ *
+ * The human_controller component...
+ *
+ * \section interface_sec Interface
+ *
+ * interface...
+ *
+ * \section install_sec Installation
+ *
+ * \subsection install1_ssec Software depencences
+ * ...
+ *
+ * \subsection install2_ssec Compile and install
+ * cd human_controller
+ * <br>
+ * cmake . && make
+ * <br>
+ * To install:
+ * <br>
+ * sudo make install
+ *
+ * \section guide_sec User guide
+ *
+ * \subsection config_ssec Configuration file
+ *
+ * <p>
+ * The configuration file etc/config...
+ * </p>
+ *
+ * \subsection execution_ssec Execution
+ *
+ * Just: "${PATH_TO_BINARY}/human_controller --Ice.Config=${PATH_TO_CONFIG_FILE}"
+ *
+ * \subsection running_ssec Once running
+ *
+ * ...
+ *
+ */
+#include <signal.h>
+
+// QT includes
+#include <QtCore>
+#include <QtGui>
+
+// ICE includes
+#include <Ice/Ice.h>
+#include <IceStorm/IceStorm.h>
+#include <Ice/Application.h>
+
+#include <rapplication/rapplication.h>
+#include <sigwatch/sigwatch.h>
+#include <qlog/qlog.h>
+
+#include "config.h"
+#include "genericmonitor.h"
+#include "genericworker.h"
+#include "specificworker.h"
+#include "specificmonitor.h"
+#include "commonbehaviorI.h"
+
+
+#include <Laser.h>
+#include <GenericBase.h>
+#include <TrajectoryRobot2D.h>
+
+
+// User includes here
+
+// Namespaces
+using namespace std;
+using namespace RoboCompCommonBehavior;
+
+class human_controller : public RoboComp::Application
+{
+public:
+	human_controller (QString prfx) { prefix = prfx.toStdString(); }
+private:
+	void initialize();
+	std::string prefix;
+	MapPrx mprx;
+
+public:
+	virtual int run(int, char*[]);
+};
+
+void ::human_controller::initialize()
+{
+	// Config file properties read example
+	// configGetString( PROPERTY_NAME_1, property1_holder, PROPERTY_1_DEFAULT_VALUE );
+	// configGetInt( PROPERTY_NAME_2, property1_holder, PROPERTY_2_DEFAULT_VALUE );
+}
+
+int ::human_controller::run(int argc, char* argv[])
+{
+#ifdef USE_QTGUI
+	QApplication a(argc, argv);  // GUI application
+#else
+	QCoreApplication a(argc, argv);  // NON-GUI application
+#endif
+
+
+	sigset_t sigs;
+	sigemptyset(&sigs);
+	sigaddset(&sigs, SIGHUP);
+	sigaddset(&sigs, SIGINT);
+	sigaddset(&sigs, SIGTERM);
+	sigprocmask(SIG_UNBLOCK, &sigs, 0);
+
+	UnixSignalWatcher sigwatch;
+	sigwatch.watchForSignal(SIGINT);
+	sigwatch.watchForSignal(SIGTERM);
+	QObject::connect(&sigwatch, SIGNAL(unixSignal(int)), &a, SLOT(quit()));
+
+	int status=EXIT_SUCCESS;
+
+	LaserPrx laser_proxy;
+	TrajectoryRobot2DPrx trajectoryrobot2d_proxy;
+	NavigatorPrx navigator_proxy;
+
+	string proxy, tmp;
+	initialize();
+
+
+	try
+	{
+		if (not GenericMonitor::configGetString(communicator(), prefix, "LaserProxy", proxy, ""))
+		{
+			cout << "[" << PROGRAM_NAME << "]: Can't read configuration for proxy LaserProxy\n";
+		}
+		laser_proxy = LaserPrx::uncheckedCast( communicator()->stringToProxy( proxy ) );
+	}
+	catch(const Ice::Exception& ex)
+	{
+		cout << "[" << PROGRAM_NAME << "]: Exception: " << ex;
+		return EXIT_FAILURE;
+	}
+	rInfo("LaserProxy initialized Ok!");
+	mprx["LaserProxy"] = (::IceProxy::Ice::Object*)(&laser_proxy);//Remote server proxy creation example
+
+
+	try
+	{
+		if (not GenericMonitor::configGetString(communicator(), prefix, "TrajectoryRobot2DProxy", proxy, ""))
+		{
+			cout << "[" << PROGRAM_NAME << "]: Can't read configuration for proxy TrajectoryRobot2DProxy\n";
+		}
+		trajectoryrobot2d_proxy = TrajectoryRobot2DPrx::uncheckedCast( communicator()->stringToProxy( proxy ) );
+	}
+	catch(const Ice::Exception& ex)
+	{
+		cout << "[" << PROGRAM_NAME << "]: Exception: " << ex;
+		return EXIT_FAILURE;
+	}
+	rInfo("TrajectoryRobot2DProxy initialized Ok!");
+	mprx["TrajectoryRobot2DProxy"] = (::IceProxy::Ice::Object*)(&trajectoryrobot2d_proxy);//Remote server proxy creation example
+
+
+	try
+	{
+		if (not GenericMonitor::configGetString(communicator(), prefix, "NavigatorProxy", proxy, ""))
+		{
+			cout << "[" << PROGRAM_NAME << "]: Can't read configuration for proxy NavigatorProxy\n";
+		}
+		navigator_proxy = NavigatorPrx::uncheckedCast( communicator()->stringToProxy( proxy ) );
+	}
+	catch(const Ice::Exception& ex)
+	{
+		cout << "[" << PROGRAM_NAME << "]: Exception: " << ex;
+		return EXIT_FAILURE;
+	}
+	rInfo("NavigatorProxy initialized Ok!");
+	mprx["NavigatorProxy"] = (::IceProxy::Ice::Object*)(&navigator_proxy);//Remote server proxy creation example
+
+
+
+	SpecificWorker *worker = new SpecificWorker(mprx);
+	//Monitor thread
+	SpecificMonitor *monitor = new SpecificMonitor(worker,communicator());
+	QObject::connect(monitor, SIGNAL(kill()), &a, SLOT(quit()));
+	QObject::connect(worker, SIGNAL(kill()), &a, SLOT(quit()));
+	monitor->start();
+
+	if ( !monitor->isRunning() )
+		return status;
+	
+	while (!monitor->ready)
+	{
+		usleep(10000);
+	}
+	
+	try
+	{
+		// Server adapter creation and publication
+		if (not GenericMonitor::configGetString(communicator(), prefix, "CommonBehavior.Endpoints", tmp, ""))
+		{
+			cout << "[" << PROGRAM_NAME << "]: Can't read configuration for proxy CommonBehavior\n";
+		}
+		Ice::ObjectAdapterPtr adapterCommonBehavior = communicator()->createObjectAdapterWithEndpoints("commonbehavior", tmp);
+		CommonBehaviorI *commonbehaviorI = new CommonBehaviorI(monitor );
+		adapterCommonBehavior->add(commonbehaviorI, communicator()->stringToIdentity("commonbehavior"));
+		adapterCommonBehavior->activate();
+
+
+
+
+
+
+
+		// Server adapter creation and publication
+		cout << SERVER_FULL_NAME " started" << endl;
+
+		// User defined QtGui elements ( main window, dialogs, etc )
+
+#ifdef USE_QTGUI
+		//ignoreInterrupt(); // Uncomment if you want the component to ignore console SIGINT signal (ctrl+c).
+		a.setQuitOnLastWindowClosed( true );
+#endif
+		// Run QT Application Event Loop
+		a.exec();
+		
+		
+		status = EXIT_SUCCESS;
+	}
+	catch(const Ice::Exception& ex)
+	{
+		status = EXIT_FAILURE;
+
+		cout << "[" << PROGRAM_NAME << "]: Exception raised on main thread: " << endl;
+		cout << ex;
+
+#ifdef USE_QTGUI
+		a.quit();
+#endif
+		monitor->exit(0);
+}
+
+	return status;
+}
+
+int main(int argc, char* argv[])
+{
+	string arg;
+
+	// Set config file
+	std::string configFile = "config";
+	if (argc > 1)
+	{
+		std::string initIC("--Ice.Config=");
+		size_t pos = std::string(argv[1]).find(initIC);
+		if (pos == 0)
+		{
+			configFile = std::string(argv[1]+initIC.size());
+		}
+		else
+		{
+			configFile = std::string(argv[1]);
+		}
+	}
+
+	// Search in argument list for --prefix= argument (if exist)
+	QString prefix("");
+	QString prfx = QString("--prefix=");
+	for (int i = 2; i < argc; ++i)
+	{
+		arg = argv[i];
+		if (arg.find(prfx.toStdString(), 0) == 0)
+		{
+			prefix = QString::fromStdString(arg).remove(0, prfx.size());
+			if (prefix.size()>0)
+				prefix += QString(".");
+			printf("Configuration prefix: <%s>\n", prefix.toStdString().c_str());
+		}
+	}
+	::human_controller app(prefix);
+
+	return app.main(argc, argv, configFile.c_str());
+}
+
